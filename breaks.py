540331e1c9c771424df8cca22b5e7b45d50e763d
Justin Ravago <50065181+jravago-dev@users.noreply.github.com>
2023-11-09 11:18:39 +0800

    Added 2 python files from day 24 snippets

---
+++ breaks.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+
+for eachFruit in fruits:
+  print(eachFruit)
+  if eachFruit == "banana":
+    break
+
+for eachFruit in fruits:
+  if eachFruit == "banana":
+    break
+  print(eachFruit)
---
+++ for-loop.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+  print(fruit)
+
+firstName = "Justin"
+
+for eachletter in firstName:
+  print(eachletter)
