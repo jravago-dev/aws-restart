d1d1232fff0061edd53a866564dafebc2bf804b0
Justin Ravago <50065181+jravago-dev@users.noreply.github.com>
2023-11-09 11:28:17 +0800

    main: added new files

---
+++ continue.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "banana", "cherry"]
+
+for eachFruit in fruits:
+  if eachFruit == "banana":
+    continue
+  print(eachFruit)
---
+++ range.py
@@ -0,0 +1,8 @@
+for number in range(2):
+  print(number)
+  
+for number in range(2,30):
+  print(number)
+
+for number in range(2,30,3):
+  print(number)
